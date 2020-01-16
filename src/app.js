c0532ecd7476a6499701ca1101cfae7c592213ae
Christina Chapman <cmchapman75@gmail.com>
2020-01-16 16:22:21 -0600

    initial commit

--- src/app.js
+++ src/app.js
@@ -5,31 +5,26 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+})); 
 
-app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
+
+app.use(bookmarksRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error'} };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
-
+app.use(errorHandler);
 
 module.exports = app;
